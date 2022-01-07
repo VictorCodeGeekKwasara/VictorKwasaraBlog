850f3f714cac3e7798e5bac154ab57e1dd45e8b9
root <root@DESKTOP-87DPNVK.localdomain>
2022-01-07 12:16:01 +0200

    blogs template done

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
 
-export default function Test() {
+export default function Test({data}) {
+const image = getImage(data.markdownRemark.frontmatter.blogImage)
+  console.log(data)
   return (
     <div>
-     
+     <GatsbyImage image={image} alt="stuff" />
     </div>
   )
 }
+
+export const query = graphql`
+  {
+    markdownRemark {
+      frontmatter {
+        blogImage {
+          childImageSharp {
+            gatsbyImageData(placeholder: TRACED_SVG)
+          }
+        }
+        snipImage {
+          childImageSharp {
+            gatsbyImageData(placeholder: TRACED_SVG)
+          }
+        }
+      }
+    }
+  }
+`
--- src/templates/blogs-list.js
+++ src/templates/blogs-list.js
@@ -2,10 +2,11 @@ import React from 'react'
 import {graphql, Link} from 'gatsby' ;
 import Layout from '../components/Layout';
 import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 
 export default function Blogs({data, pageContext}) {
-
+  
    console.log(data)
   const previousPage = pageContext.currentPage === 2 ? '/blog': `/blog/${pageContext.currentPage-1}`;
   const nextPage = `/blog/${pageContext.currentPage + 1}`;
@@ -18,16 +19,21 @@ export default function Blogs({data, pageContext}) {
       <ul>
 
         {
-          data.allMarkdownRemark.edges.map(blog=>(
-                  <li key={blog.node.id}> 
+          data.allMarkdownRemark.edges.map(blog=>{
+            
+            const image = getImage(blog.node.frontmatter.snipImage)
+            return   <li key={blog.node.id}> 
                   <Link to={blog.node.fields.slug}>
+                     <GatsbyImage image={image} alt="stuff" />
                       <h1>{blog.node.frontmatter.title}</h1>
                       <h3>{blog.node.frontmatter.date}</h3>
                       <p>{blog.node.excerpt}</p>
                    </Link> 
-                   </li>    
+                   </li>    }
+            
+           
 
-          ))
+          )
         }
       </ul>
 
@@ -48,28 +54,32 @@ export default function Blogs({data, pageContext}) {
 }
 
 export const query = graphql`
-  query BlogListQuery($skip: Int!, $limit: Int!){
-
-    allMarkdownRemark(
-      sort:{fields:[frontmatter___date], order: DESC}
-      filter: {frontmatter: {contentKey: {eq: "blog"}}}
-      limit: $limit
-      skip: $skip
-    ){
-      edges {
-        node{
-          id
-          frontmatter{
-            title
-            date(formatString: "MMMM D, YYYY")
-            
+  query BlogListQuery($skip: Int!, $limit: Int!) {
+  allMarkdownRemark(
+    sort: {fields: [frontmatter___date], order: DESC}
+    filter: {frontmatter: {contentKey: {eq: "blog"}}}
+    limit: $limit
+    skip: $skip
+  ) {
+    edges {
+      node {
+        id
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+          snipImage {
+            childImageSharp {
+              gatsbyImageData(placeholder: TRACED_SVG)
+            }
           }
-          fields{
-            slug
-          }
-          excerpt
         }
+        fields {
+          slug
+        }
+        excerpt
       }
     }
   }
+}
+
 `
